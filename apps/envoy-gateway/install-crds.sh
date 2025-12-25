db1f5e8ec4f1c169440a03c72e7c5c404b100648
vabalops <220776952+vabalops@users.noreply.github.com>
2025-12-25 23:00:46 +0200

    deploy pihole

--- apps/envoy-gateway/install-crds.sh
+++ apps/envoy-gateway/install-crds.sh
@@ -1,3 +1,7 @@
 # https://gateway-api.sigs.k8s.io/guides/getting-started/#installing-gateway-api
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
 
+# experimental for TCP and UDP rules
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/experimental-install.yaml
+
+
