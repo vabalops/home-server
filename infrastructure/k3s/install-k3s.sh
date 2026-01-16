6b097b8bddc979276ac1ad76285138af014b56f9
vabalops <220776952+vabalops@users.noreply.github.com>
2026-01-16 12:00:35 +0200

    add ha k3s commands

--- infrastructure/k3s/install-k3s.sh
+++ infrastructure/k3s/install-k3s.sh
@@ -16,3 +16,14 @@ curl -sfL https://get.k3s.io | K3S_URL='https://192.168.1.121:6443' K3S_TOKEN=${
 add roles to nodes:
 kubectl label node --all node-role.kubernetes.io/worker=
 kubectl label node --all node-role.kubernetes.io/longhorn=
+
+
+###### HA
+
+# requires load balancer in front of servers
+
+# first node
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.34 sh -s - --cluster-init --token homeKubernetes2026 --tls-san 192.168.1.222 --disable=traefik --disable=local-storage --flannel-backend=host-gw --disable-network-policy
+
+# remaining
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.34 sh -s - --server https://192.168.1.222:6443 --token homeKubernetes2026 --disable=traefik --disable=local-storage --flannel-backend=host-gw --disable-network-policy
